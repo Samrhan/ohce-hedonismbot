18150d97f4ae4473cb4c0a3150a199fa0b7d4209
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-15 08:44:03 +0100

    refacto(greeter): Refactor greeter tests

--- js/greeter.js
+++ js/greeter.js
@@ -12,14 +12,14 @@ class Greeter {
 
   greet () {
     const currentHour = this.clock.currentHour()
-    if (currentHour <= 6 && currentHour < 12) {
+    if (currentHour > 20 || currentHour <= 6) {
+      return 'Good night'
+    }
+    if (currentHour >= 6 && currentHour < 12) {
       return 'Good morning'
     }
     if (currentHour >= 12 && currentHour <= 19) {
       return 'Good afternoon'
-    }
-    if (currentHour > 20 || currentHour <= 6) {
-      return 'Good night'
     }
   }
 }
